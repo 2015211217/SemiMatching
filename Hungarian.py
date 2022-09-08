627b2563cb676b232cc90ae29998ff86944e05f2
JiangXuanke <15223370366@163.com>
2022-09-08 23:25:45 +0900

    9.8

--- Hungarian.py
+++ Hungarian.py
@@ -14,10 +14,14 @@ def HungarianMaxMatching(graph):
         if graph.getDegree(graph, i, 0) == 0: # if the vertex is isolated, then skip
             continue
         Q.put([i, 0])
+        bestV = -1
         while not Q.empty():
             w = Q.get()
             N = []
             if w[1] == 0: #w is in the left side
-                N = graph.getUnmatchedNeighbor(graph, w[0], )
+                N = graph.getUnmatchedNeighbor(graph, w[0], w[1])
             else:
-
+                N = graph.getMatchedNeighbor(graph, w[0], w[1])
+                if bestV == -1 or graph.getDegreeM(graph, bestV, 1) < graph.getDegreeM(graph, w[0], 1):
+                    bestV = w[0]
+                
